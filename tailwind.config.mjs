4d7c1ee81f102382226494ab4ccba857047f0112
Farhan Fadila <farhan.fadila1717@gmail.com>
2025-01-30 16:35:40 +0700

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'pallate-1': '#051367',
+				'pallate-2': '#4B3EC1',
+				'pallate-3': '#5D8BF4',
+				'pallate-4': '#DFF6FF',
+
+			},
+		},
+	},
+	plugins: [],
+}
