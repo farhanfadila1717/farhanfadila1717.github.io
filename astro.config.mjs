78489a906457e6917f228b4f2a68cec2a2f659ab
farhanfadila1717 <farhan.fadila1717@gmail.com>
2025-01-31 21:24:38 +0700

    fix: remove base URL in Astro configuration for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://farhanfadila1717.github.io',
-  base: 'farhanfadila1717.github.io',
+  base: '',
   integrations: [tailwind()],
   experimental: {
     svg: true,
