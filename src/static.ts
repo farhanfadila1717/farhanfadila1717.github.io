0d2500703efb4c1e5e7ef0ac591d307e0a662d20
farhanfadila1717 <farhan.fadila1717@gmail.com>
2025-01-31 20:21:28 +0700

    feat: complete website

---
+++ src/static.ts
@@ -0,0 +1,5 @@
+
+export const AVATAR_URL = 'https://avatars.githubusercontent.com/u/43161050?v=4';
+export const GITHUB_URL = 'https://github.com/farhanfadila1717';
+export const LINKEDIN_URL = 'https://linkedin.com/in/farhan-fadila-b4b008186';
+export const MAIL_TO = 'mailto:farhan.fadila1717@gmail.com';
