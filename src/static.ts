03e751ab20d8ab8fb67c13e3c39799f46f19ea41
farhanfadila1717 <farhan.fadila1717@gmail.com>
2025-01-31 21:08:24 +0700

    feat: add CV URL to static file

--- src/static.ts
+++ src/static.ts
@@ -3,3 +3,4 @@ export const AVATAR_URL = 'https://avatars.githubusercontent.com/u/43161050?v=4'
 export const GITHUB_URL = 'https://github.com/farhanfadila1717';
 export const LINKEDIN_URL = 'https://linkedin.com/in/farhan-fadila-b4b008186';
 export const MAIL_TO = 'mailto:farhan.fadila1717@gmail.com';
+export const CV_URL = 'https://raw.githubusercontent.com/farhanfadila1717/portofolio-data/refs/heads/main/pdf/CV-Farhan%20Fadila-Flutter%20Developer.pdf';
