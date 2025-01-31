ab94c077c58f0198499e6fad1ce8d3eb801d2a5e
farhanfadila1717 <farhan.fadila1717@gmail.com>
2025-01-31 21:08:40 +0700

    feat: set site and base URL for personal website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://farhanfadila1717.github.io',
+  base: 'personal-website',
   integrations: [tailwind()],
   experimental: {
     svg: true,
